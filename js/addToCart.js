694f9f479ebacaeb67795cc5c6d25710787b607a
DressedHuman <dressed.human@gmail.com>
2023-08-22 01:22:01 +0600

    added some functionality like  adding products to cart using js

---
+++ js/addToCart.js
@@ -0,0 +1,70 @@
+// helper function for the main (addToCart) function to add the name to the cart
+function addToCartItem(productName){
+    const cart = document.getElementById("cart-items");
+    const itemsCount = cart.childElementCount;
+    const newItem = document.createElement("p");
+    newItem.classList.add("text-[var(--black-color)]", "font-medium", "text-2xl", "mb-4");
+    newItem.innerText = `${parseInt(itemsCount+1)}. ${productName}`;
+
+    // adding to the cart list
+    cart.appendChild(newItem);
+};
+
+// new value counter: helper function for the addToCartPrice function
+function addValue(prevValue, newValue){
+    value = parseFloat(prevValue) + parseFloat(newValue);
+    return parseFloat(value.toFixed(2));
+}
+
+// helper function to adjust the price
+function addToCartPrice(productPrice){
+    // setting the total price
+    const priceTotal = document.getElementById("price-total");
+    const newPriceTotal = addValue(priceTotal.innerText, productPrice);
+    priceTotal.innerText = newPriceTotal;
+    if(newPriceTotal >=200){
+        document.getElementById("btn-apply-coupon").removeAttribute("disabled");
+    }
+
+    // setting the total price with the discount counted
+    const total = document.getElementById("total");
+    total.innerText = addValue(total.innerText, productPrice);
+};
+
+
+// the main function to add a product to the cart
+function addToCart(productName, productPrice){
+    addToCartItem(productName);
+    addToCartPrice(productPrice);
+}
+
+
+// adding event listener to the products using for loop
+
+// storing the names and prices of the products to use in the for loop when adding event listener
+const itemsNamePrice = {
+    product_1 : {name : "K. Accessories", price : "39.00"},
+    product_2 : {name : "K. Accessories", price : "25.00"},
+    product_3 : {name : "Home Cooker", price : "49.00"},
+    product_4 : {name : "Sports Back Cap", price : "49.00"},
+    product_5 : {name : "Full Jersey Set", price : "69.00"},
+    product_6 : {name : "Sports cates", price : "159.00"},
+}
+
+const keys = Object.keys(itemsNamePrice);
+
+for(const key of keys){
+    document.getElementById(key).addEventListener("click", function(event){
+        const object = itemsNamePrice[key];
+        addToCart(object.name, object.price);
+        event.stopPropagation();
+    });
+};
+
+/* document.getElementById("product-1").addEventListener("click", function(){
+    addToCart("K. Accessories", 39);
+}, true); */
+
+document.getElementById("btn-apply-coupon").addEventListener("click", function(){
+    alert("apply button clicked");
+})
